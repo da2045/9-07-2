7c8ae7c73acdd3c5878687006f4a5a952db85ba8
da2045 <vitde2009@gmail.com>
2024-08-14 16:45:34 +0300

    Add files via upload

---
+++ dz6.py
@@ -0,0 +1,37 @@
+# expression = input("Введіть арифметичний вираз --> ")
+
+# num1, operator, num2 = expression[0], expression[1], expression[2:]
+
+# num1 = int(num1)
+# num2 = int(num2)
+
+# if operator == '+':
+#     result = num1 + num2
+# elif operator == '-':
+#     result = num1 - num2
+# elif operator == '*':
+#     result = num1 * num2
+# elif operator == '/':
+#     result = num1 / num2
+
+# print("Результат виразу:", result)
+
+
+
+
+# import random
+# numbers = [random.randint(-10, 10) for _ in range(20)]
+
+# min_element = min(numbers)
+# max_element = max(numbers)
+
+# negative_count = sum(1 for x in numbers if x < 0)
+# positive_count = sum(1 for x in numbers if x > 0)
+# zero_count = numbers.count(0)
+
+# print("Список чисел:", numbers)
+# print("Мінімальний елемент:", min_element)
+# print("Максимальний елемент:", max_element)
+# print("Кількість від'ємних елементів:", negative_count)
+# print("Кількість додатних елементів:", positive_count)
+# print("Кількість нулів:", zero_count)
